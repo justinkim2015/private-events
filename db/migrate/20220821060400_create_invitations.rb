155213cfe89eb979b5b4200272ab78ee510e49c6
justinkim2015 <justinkim2015@gmail.com>
2022-08-21 18:14:16 +0900

    Remove invitation table

--- db/migrate/20220821060400_create_invitations.rb
+++
@@ -1,8 +0,0 @@
-class CreateInvitations < ActiveRecord::Migration[7.0]
-  def change
-    create_table :invitations do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -29,11 +29,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_21_060400) do
     t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
-  create_table "invitations", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false

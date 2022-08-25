ab6806cb410c59352e115992db7395f26c97b10c
justinkim2015 <justinkim2015@gmail.com>
2022-08-25 15:48:12 +0900

    Finish invite functionality

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -14,26 +14,22 @@ class InvitesController < ApplicationController
     @invite = Invite.find(params[:id])
     @invite.destroy
 
-    flash[:notice] = 'Uninvited!'
-    redirect_to :back
-    # also testing the above to see if it works.
+    flash[:alert] = 'Invite deleted!'
+    redirect_back(fallback_location: root_path)
   end
 
   def accept
-    @invite = Invite.find(params[:id])
+    @invite = Invite.find(params[:invite][:invite_id])
+    EventAttendee.create(attended_event_id: params[:invite][:invited_event_id], attendee_id: current_user.id)
     @invite.destroy
 
-    flash[:notice] = 'Uninvited!'
-    redirect_to event_path(params[:id])
+    flash[:notice] = 'Accepted!'
+    redirect_to event_path(params[:invite][:invited_event_id])
   end
 
   private
 
   def invites_params
-    params.require(:invite).permit(:invitee_id, :invited_event_id)
-  end
-
-  def accept_params
-    params.require(:invite).permit(:invite_id, :invited_event_id, :accept)
+    params.require(:invite).permit(:invitee_id, :invited_event_id, :invite_id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_023418) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
